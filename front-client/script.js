36c5c1ca3cdf3e917f0c49d4fe1c743ecb680c6d
Giovanni <giovanni.carrascoa@utem.cl>
2019-06-06 02:10:19 -0400

    Se agregaron los graficos de cada sensor

--- front-client/script.js
+++ front-client/script.js
@@ -9,10 +9,18 @@
 
         }
         function tabla(datos){
-            var datos1 = [];
-            var datos2 = [];
-            var lista = [];
-            var contador =0;
+            var datos1_id1 = [];
+            var datos1_id2 = [];
+            var datos1_id3 = [];
+            var datos2_id1 = [];
+            var datos2_id2 = [];
+            var datos2_id3 = [];
+            var lista1 = [];
+            var lista2 = [];
+            var lista3 = [];
+            var contador1 =0;
+            var contador2 =0;
+            var contador3 =0;
             for (let valor of datos){    
                 contenido.innerHTML += `
                 <tr>
@@ -24,15 +32,34 @@
                  </tr>
             
                 `
-                if (contador <= 60){
-                    datos2.push(valor.valor);
-                    var fecha = new Date(valor.date);
-                    datos1.push(fecha.getTime());
-                    week_data = {"year": fecha.getTime(),"value":valor.valor};
-                    lista.push(week_data);
-                    contador++;
+                if (valor.sensor==1){
+                    datos2_id1.push(valor.valor);
+                    var fecha1 = new Date(valor.date);
+                    datos1_id1.push(fecha1.getTime());
+                    week_data_id1 = {"year": fecha1.getTime(),"value":valor.valor};
+                    lista1.push(week_data_id1);
+                    contador1++;
                     // console.log(contador);
                 }
+                if (valor.sensor==2){
+                    datos2_id2.push(valor.valor);
+                    var fecha2 = new Date(valor.date);
+                    datos1_id2.push(fecha2.getTime());
+                    week_data_id2 = {"year": fecha2.getTime(),"value":valor.valor};
+                    lista2.push(week_data_id2);
+                    contador2++;
+                    // console.log(contador);
+                }
+                if (valor.sensor==3){
+                    datos2_id3.push(valor.valor);
+                    var fecha3 = new Date(valor.date);
+                    datos1_id3.push(fecha3.getTime());
+                    week_data_id3 = {"year": fecha3.getTime(),"value":valor.valor};
+                    lista3.push(week_data_id3);
+                    contador3++;
+                    // console.log(contador);
+                }
+                
                 // datos2.push(valor.valor);
                 // var fecha = new Date(valor.date);
                 // datos1.push(fecha.getTime());
@@ -42,15 +69,19 @@
                 
                 
             }   
-            console.log(lista);
-            grafico(lista);
+            console.log(lista1);
+            grafico(lista1,"myfirstchart1");
+            console.log(lista2);
+            grafico(lista2,"myfirstchart2");
+            console.log(lista3);
+            grafico(lista3,"myfirstchart3");
             
         }
         
-        function grafico(lista){
+        function grafico(lista,nombrechart){
             new Morris.Line({
             // ID of the element in which to draw the chart.
-            element: 'myfirstchart',
+            element: nombrechart,
             // Chart data records -- each entry in this array corresponds to a point on
             // the chart.
                     
