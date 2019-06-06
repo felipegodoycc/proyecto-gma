13382e62f7910c36f1f606d20544a59a365e9742
Giovanni <giovanni.carrascoa@utem.cl>
2019-06-06 16:53:51 -0400

    Se agrego la opcion de fechas para graficar . falta validar campos

--- front-client/script.js
+++ front-client/script.js
@@ -1,4 +1,6 @@
-        var contenido = document.querySelector("#contenido")
+        var contenido = document.querySelector("#contenido");
+        var fechaInicio = document.getElementById("fechaInicio");
+        var fechaTermino = document.getElementById("fechaTermino");
         function traer(){
             fetch("https://us-central1-gma-api-rest.cloudfunctions.net/api/mediciones")
             .then(res => res.json())
@@ -9,6 +11,8 @@
 
         }
         function tabla(datos){
+            fechaInicio= new Date(fechaInicio.value);
+            fechaTermino = new Date(fechaTermino.value);
             var datos1_id1 = [];
             var datos1_id2 = [];
             var datos1_id3 = [];
@@ -21,7 +25,11 @@
             var contador1 =0;
             var contador2 =0;
             var contador3 =0;
+            var fechaInicioAux = new Date(fechaInicio);
+            var fechaTerminoAux = new Date(fechaTermino);
+            console.log(fechaInicioAux.getTime());
             for (let valor of datos){    
+                
                 contenido.innerHTML += `
                 <tr>
                             <th scope="row">${valor._id}</th>
@@ -32,32 +40,47 @@
                  </tr>
             
                 `
+
                 if (valor.sensor==1){
+                    
                     datos2_id1.push(valor.valor);
                     var fecha1 = new Date(valor.date);
-                    datos1_id1.push(fecha1.getTime());
-                    week_data_id1 = {"year": fecha1.getTime(),"value":valor.valor};
-                    lista1.push(week_data_id1);
-                    contador1++;
+                    console.log(fecha1.getTime());
+                    if ( fecha1.getTime()>= fechaInicioAux.getTime() && fecha1.getTime()<=fechaTerminoAux.getTime() ) {
+                        datos1_id1.push(fecha1.getTime());
+                        week_data_id1 = {"year": fecha1.getTime(),"value":valor.valor};
+                        lista1.push(week_data_id1);
+                        contador1++;
+
+                    }
+                    
+                    
                     // console.log(contador);
                 }
                 if (valor.sensor==2){
+
                     datos2_id2.push(valor.valor);
                     var fecha2 = new Date(valor.date);
+
+                    if ( fecha2.getTime()>= fechaInicioAux.getTime() && fecha2.getTime()<=fechaTerminoAux.getTime() ) {
                     datos1_id2.push(fecha2.getTime());
                     week_data_id2 = {"year": fecha2.getTime(),"value":valor.valor};
                     lista2.push(week_data_id2);
                     contador2++;
                     // console.log(contador);
+                    }
                 }
                 if (valor.sensor==3){
                     datos2_id3.push(valor.valor);
                     var fecha3 = new Date(valor.date);
+
+                    if ( fecha3.getTime()>= fechaInicioAux.getTime() && fecha3.getTime()<=fechaTerminoAux.getTime() ) {
                     datos1_id3.push(fecha3.getTime());
                     week_data_id3 = {"year": fecha3.getTime(),"value":valor.valor};
                     lista3.push(week_data_id3);
                     contador3++;
                     // console.log(contador);
+                    }
                 }
                 
                 // datos2.push(valor.valor);
@@ -69,11 +92,11 @@
                 
                 
             }   
-            console.log(lista1);
+            // console.log(lista1);
             grafico(lista1,"myfirstchart1");
-            console.log(lista2);
+            // console.log(lista2);
             grafico(lista2,"myfirstchart2");
-            console.log(lista3);
+            // console.log(lista3);
             grafico(lista3,"myfirstchart3");
             
         }
